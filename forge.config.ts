f4476d8045e6ac27d33a7bb0fbc23b8ca6f028e2
JonathanLab <jonathanmieloo@gmail.com>
2025-10-20 16:57:50 +0200

    add makerdmg

--- forge.config.ts
+++ forge.config.ts
@@ -1,3 +1,4 @@
+import { MakerDMG } from "@electron-forge/maker-dmg";
 import { MakerZIP } from "@electron-forge/maker-zip";
 import { VitePlugin } from "@electron-forge/plugin-vite";
 import type { ForgeConfig } from "@electron-forge/shared-types";
@@ -10,7 +11,13 @@ const config: ForgeConfig = {
     icon: "./build/app-icon", // Forge adds .icns/.ico/.png based on platform
   },
   rebuildConfig: {},
-  makers: [new MakerZIP({}, ["darwin", "linux", "win32"])],
+  makers: [
+    new MakerDMG({
+      icon: "./build/app-icon.icns",
+      format: "ULFO",
+    }),
+    new MakerZIP({}, ["darwin", "linux", "win32"]),
+  ],
   plugins: [
     new VitePlugin({
       build: [
