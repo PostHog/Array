8834f82fc0143464a7ab45aade6072f83948255f
JonathanLab <jonathanmieloo@gmail.com>
2025-10-22 18:51:56 +0200

    undo hackfix

--- src/main/services/recording.ts
+++ src/main/services/recording.ts
@@ -12,29 +12,26 @@ import {
   TASK_EXTRACTION_PROMPT,
 } from "./transcription-prompts.js";
 
-const fallbackFile = class File extends Blob {
-  name: string;
-  lastModified: number;
+let FileConstructor: typeof File;
+try {
+  const { File: NodeFile } = await import("node:buffer");
+  FileConstructor = NodeFile as typeof File;
+} catch {
+  FileConstructor = class File extends Blob {
+    name: string;
+    lastModified: number;
 
-  constructor(bits: BlobPart[], name: string, options?: FilePropertyBag) {
-    super(bits, options);
-    this.name = name;
-    this.lastModified = options?.lastModified ?? Date.now();
-  }
-} as typeof File;
+    constructor(bits: BlobPart[], name: string, options?: FilePropertyBag) {
+      super(bits, options);
+      this.name = name;
+      this.lastModified = options?.lastModified ?? Date.now();
+    }
+  } as typeof File;
+}
 
-(async () => {
-  let FileConstructor: typeof File;
-  try {
-    FileConstructor = (await import("node:buffer")).File as typeof File;
-  } catch {
-    FileConstructor = fallbackFile;
-  }
-
-  if (!globalThis.File) {
-    globalThis.File = FileConstructor;
-  }
-})();
+if (!globalThis.File) {
+  globalThis.File = FileConstructor;
+}
 
 interface RecordingSession {
   id: string;
