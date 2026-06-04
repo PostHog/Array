szymeo/code

feat(signals): add team default notification channel to inbox config
Expose the team-level default Slack channel
(SignalTeamConfig.slack_notification_channel) in the inbox
configuration, editable by org admins, alongside the existing per-user
notification channel. Both options are explained in the UI.

Extracts a shared SlackChannelCombobox so the team-default and per-user
pickers reuse one implementation. The team channel lists from the team's
first slack integration, matching how the backend resolves the
integration at send time.

Co-Authored-By: Claude Opus 4.8 (1M context) <noreply@anthropic.com>

apps/code/src/renderer/api/posthogClient.ts
@@ -2214,9 +2214,12 @@ export class PostHogAPIClient {
     return (await response.json()) as SignalTeamConfig;
   }
 
-  async updateSignalTeamConfig(updates: {
-    default_autostart_priority: string;
-  }): Promise<SignalTeamConfig> {
+  async updateSignalTeamConfig(
+    updates: Partial<{
+      default_autostart_priority: string;
+      slack_notification_channel: string | null;
+    }>,
+  ): Promise<SignalTeamConfig> {
     const teamId = await this.getTeamId();
     const url = new URL(
       `${this.api.baseUrl}/api/projects/${teamId}/signals/config/`,

apps/code/src/renderer/features/inbox/hooks/useSignalSourceManager.ts
@@ -477,6 +477,27 @@ export function useSignalSourceManager() {
     [client, queryClient],
   );
 
+  const handleUpdateTeamSlackChannel = useCallback(
+    async (channel: string | null) => {
+      if (!client) return;
+      try {
+        await client.updateSignalTeamConfig({
+          slack_notification_channel: channel,
+        });
+        await queryClient.invalidateQueries({
+          queryKey: ["signals", "team-config"],
+        });
+      } catch (error: unknown) {
+        const message =
+          error instanceof Error
+            ? error.message
+            : "Failed to update default notification channel";
+        toast.error(message);
+      }
+    },
+    [client, queryClient],
+  );
+
   const handleUpdateUserAutonomyPriority = useCallback(
     async (priority: string | null) => {
       if (!client) return;
@@ -591,6 +612,7 @@ export function useSignalSourceManager() {
     handleToggleEvaluation,
     teamConfig,
     handleUpdateAutostartPriority,
+    handleUpdateTeamSlackChannel,
     userAutonomyConfig,
     userAutonomyConfigLoading,
     handleUpdateUserAutonomyPriority,

apps/code/src/renderer/features/settings/components/sections/SignalDefaultChannelSettings.tsx
@@ -1,0 +1,84 @@
+import { useIsOrgAdmin } from "@features/auth/hooks/useOrgRole";
+import { useSignalSourceManager } from "@features/inbox/hooks/useSignalSourceManager";
+import { useIntegrationSelectors } from "@features/integrations/stores/integrationStore";
+import { SlackChannelCombobox } from "@features/settings/components/SlackChannelCombobox";
+import { Box, Flex, Text } from "@radix-ui/themes";
+
+interface SignalDefaultChannelSettingsProps {
+  channelComboboxModal?: boolean;
+  isLoading?: boolean;
+}
+
+export function SignalDefaultChannelSettings({
+  channelComboboxModal = false,
+  isLoading = false,
+}: SignalDefaultChannelSettingsProps) {
+  const { slackIntegrations, hasSlackIntegration } = useIntegrationSelectors();
+  const { teamConfig, handleUpdateTeamSlackChannel } = useSignalSourceManager();
+  const { isAdmin } = useIsOrgAdmin();
+
+  // The backend resolves the team's Slack integration dynamically (its first
+  // `slack` install), so we list channels from that same workspace.
+  const teamIntegrationId = slackIntegrations[0]?.id ?? null;
+  const channelTarget = teamConfig?.slack_notification_channel ?? null;
+  const canEdit = isAdmin === true;
+
+  if (isLoading) {
+    return (
+      <Flex
+        direction="column"
+        gap="2"
+        pt="3"
+        className="border-(--gray-5) border-t border-dashed"
+      >
+        <Flex direction="column" gap="1">
+          <Box className="h-[14px] w-[200px] animate-pulse rounded bg-gray-4" />
+          <Box className="h-[11px] w-[80%] animate-pulse rounded bg-gray-3" />
+        </Flex>
+        <Box className="mt-1 h-[28px] w-[200px] animate-pulse rounded bg-gray-3" />
+      </Flex>
+    );
+  }
+
+  // Connecting Slack is offered in the per-user section below; nothing to
+  // configure here until a workspace exists.
+  if (!hasSlackIntegration) return null;
+
+  return (
+    <Flex
+      direction="column"
+      gap="2"
+      pt="3"
+      style={{ borderTop: "1px dashed var(--gray-5)" }}
+    >
+      <Flex direction="column" gap="1">
+        <Text className="font-medium text-(--gray-12) text-sm">
+          Default notification channel
+        </Text>
+        <Text className="text-(--gray-11) text-[13px]">
+          Every new inbox report for the team is posted to this channel, with
+          the suggested reviewers @mentioned. Shared across the whole team.
+        </Text>
+      </Flex>
+
+      <Flex direction="column" gap="1" className="min-w-0">
+        <Text className="text-(--gray-11) text-[12px]">Channel</Text>
+        <SlackChannelCombobox
+          integrationId={teamIntegrationId}
+          value={channelTarget}
+          onChange={(channel) => void handleUpdateTeamSlackChannel(channel)}
+          offLabel="No default channel"
+          ariaLabel="Default notification channel"
+          modal={channelComboboxModal}
+          disabled={!canEdit || !teamIntegrationId}
+        />
+      </Flex>
+
+      <Text className="text-(--gray-10) text-[11px]">
+        {canEdit
+          ? "PostHog must be in the channel — invite with /invite @PostHog."
+          : "Only organization admins can change the team's default channel."}
+      </Text>
+    </Flex>
+  );
+}

apps/code/src/shared/types.ts
@@ -526,6 +526,8 @@ export interface SignalReportTask {
 export interface SignalTeamConfig {
   id: string;
   default_autostart_priority: SignalReportPriority;
+  /** Team-wide default `channel_id|#channel-name` target for inbox notifications. `null` = no team default. */
+  slack_notification_channel?: string | null;
   created_at: string;
   updated_at: string;
 }

PostHog/Array

apps/code/src/renderer/api/posthogClient.ts
@@ -2270,9 +2270,12 @@ export class PostHogAPIClient {
     return (await response.json()) as SignalTeamConfig;
   }
 
-  async updateSignalTeamConfig(updates: {
-    default_autostart_priority: string;
-  }): Promise<SignalTeamConfig> {
+  async updateSignalTeamConfig(
+    updates: Partial<{
+      default_autostart_priority: string;
+      default_slack_notification_channel: string | null;
+    }>,
+  ): Promise<SignalTeamConfig> {
     const teamId = await this.getTeamId();
     const url = new URL(
       `${this.api.baseUrl}/api/projects/${teamId}/signals/config/`,

apps/code/src/renderer/features/inbox/hooks/useSignalSourceManager.ts
@@ -477,6 +477,27 @@ export function useSignalSourceManager() {
     [client, queryClient],
   );
 
+  const handleUpdateTeamSlackChannel = useCallback(
+    async (channel: string | null) => {
+      if (!client) return;
+      try {
+        await client.updateSignalTeamConfig({
+          default_slack_notification_channel: channel,
+        });
+        await queryClient.invalidateQueries({
+          queryKey: ["signals", "team-config"],
+        });
+      } catch (error: unknown) {
+        const message =
+          error instanceof Error
+            ? error.message
+            : "Failed to update default notification channel";
+        toast.error(message);
+      }
+    },
+    [client, queryClient],
+  );
+
   const handleUpdateUserAutonomyPriority = useCallback(
     async (priority: string | null) => {
       if (!client) return;
@@ -591,6 +612,7 @@ export function useSignalSourceManager() {
     handleToggleEvaluation,
     teamConfig,
     handleUpdateAutostartPriority,
+    handleUpdateTeamSlackChannel,
     userAutonomyConfig,
     userAutonomyConfigLoading,
     handleUpdateUserAutonomyPriority,

apps/code/src/renderer/features/settings/components/sections/SignalDefaultChannelSettings.tsx
@@ -1,0 +1,84 @@
+import { useIsOrgAdmin } from "@features/auth/hooks/useOrgRole";
+import { useSignalSourceManager } from "@features/inbox/hooks/useSignalSourceManager";
+import { useIntegrationSelectors } from "@features/integrations/stores/integrationStore";
+import { SlackChannelCombobox } from "@features/settings/components/SlackChannelCombobox";
+import { Box, Flex, Text } from "@radix-ui/themes";
+
+interface SignalDefaultChannelSettingsProps {
+  channelComboboxModal?: boolean;
+  isLoading?: boolean;
+}
+
+export function SignalDefaultChannelSettings({
+  channelComboboxModal = false,
+  isLoading = false,
+}: SignalDefaultChannelSettingsProps) {
+  const { slackIntegrations, hasSlackIntegration } = useIntegrationSelectors();
+  const { teamConfig, handleUpdateTeamSlackChannel } = useSignalSourceManager();
+  const { isAdmin } = useIsOrgAdmin();
+
+  // The backend resolves the team's Slack integration dynamically (its first
+  // `slack` install), so we list channels from that same workspace.
+  const teamIntegrationId = slackIntegrations[0]?.id ?? null;
+  const channelTarget = teamConfig?.default_slack_notification_channel ?? null;
+  const canEdit = isAdmin === true;
+
+  if (isLoading) {
+    return (
+      <Flex
+        direction="column"
+        gap="2"
+        pt="3"
+        className="border-(--gray-5) border-t border-dashed"
+      >
+        <Flex direction="column" gap="1">
+          <Box className="h-[14px] w-[200px] animate-pulse rounded bg-gray-4" />
+          <Box className="h-[11px] w-[80%] animate-pulse rounded bg-gray-3" />
+        </Flex>
+        <Box className="mt-1 h-[28px] w-[200px] animate-pulse rounded bg-gray-3" />
+      </Flex>
+    );
+  }
+
+  // Connecting Slack is offered in the per-user section below; nothing to
+  // configure here until a workspace exists.
+  if (!hasSlackIntegration) return null;
+
+  return (
+    <Flex
+      direction="column"
+      gap="2"
+      pt="3"
+      style={{ borderTop: "1px dashed var(--gray-5)" }}
+    >
+      <Flex direction="column" gap="1">
+        <Text className="font-medium text-(--gray-12) text-sm">
+          Default notification channel
+        </Text>
+        <Text className="text-(--gray-11) text-[13px]">
+          Every new inbox report for the team is posted to this channel, with
+          the suggested reviewers @mentioned. Shared across the whole team.
+        </Text>
+      </Flex>
+
+      <Flex direction="column" gap="1" className="min-w-0">
+        <Text className="text-(--gray-11) text-[12px]">Channel</Text>
+        <SlackChannelCombobox
+          integrationId={teamIntegrationId}
+          value={channelTarget}
+          onChange={(channel) => void handleUpdateTeamSlackChannel(channel)}
+          offLabel="No default channel"
+          ariaLabel="Default notification channel"
+          modal={channelComboboxModal}
+          disabled={!canEdit || !teamIntegrationId}
+        />
+      </Flex>
+
+      <Text className="text-(--gray-10) text-[11px]">
+        {isAdmin === false
+          ? "Only organization admins can change the team's default channel."
+          : "PostHog must be in the channel — invite with /invite @PostHog."}
+      </Text>
+    </Flex>
+  );
+}

apps/code/src/shared/types.ts
@@ -539,6 +539,8 @@ export interface SignalReportTask {
 export interface SignalTeamConfig {
   id: string;
   default_autostart_priority: SignalReportPriority;
+  /** Team-wide default `channel_id|#channel-name` target for inbox notifications. `null` = no team default. */
+  default_slack_notification_channel?: string | null;
   created_at: string;
   updated_at: string;
 }