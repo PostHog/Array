szymeo/code

refactor: everything

packages/ui/src/features/projects/useProjectQuery.ts
@@ -1,0 +1,21 @@
+import { useAuthStateValue } from "@posthog/ui/features/auth/store";
+import { useAuthenticatedQuery } from "@posthog/ui/hooks/useAuthenticatedQuery";
+
+export function useProjectQuery() {
+  const projectId = useAuthStateValue((state) => state.projectId);
+
+  return useAuthenticatedQuery(
+    ["project", projectId],
+    async (client) => {
+      if (!projectId) {
+        throw new Error("No project ID available");
+      }
+      const data = await client.getProject(projectId);
+      return data;
+    },
+    {
+      staleTime: 5 * 60 * 1000,
+      enabled: !!projectId,
+    },
+  );
+}

PostHog/Array

packages/ui/src/features/projects/useProjectQuery.ts
@@ -1,0 +1,21 @@
+import { useAuthStateValue } from "@posthog/ui/features/auth/store";
+import { useAuthenticatedQuery } from "@posthog/ui/hooks/useAuthenticatedQuery";
+
+export function useProjectQuery() {
+  const projectId = useAuthStateValue((state) => state.currentProjectId);
+
+  return useAuthenticatedQuery(
+    ["project", projectId],
+    async (client) => {
+      if (!projectId) {
+        throw new Error("No project ID available");
+      }
+      const data = await client.getProject(projectId);
+      return data;
+    },
+    {
+      staleTime: 5 * 60 * 1000,
+      enabled: !!projectId,
+    },
+  );
+}